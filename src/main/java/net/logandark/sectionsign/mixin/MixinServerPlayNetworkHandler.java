cc2a649792289565c392212761bd5882201249f0
Logan Darklock <logandarklock@gmail.com>
2020-11-04 13:03:58 -0800

    Add support for 1.16.4
    1.16.4 changed the method of stripping sign formatting, causing the mixin to fail since it uses a method reference instead of an explicit call now. SectionSign is now compatible with both

--- src/main/java/net/logandark/sectionsign/mixin/MixinServerPlayNetworkHandler.java
+++ src/main/java/net/logandark/sectionsign/mixin/MixinServerPlayNetworkHandler.java
@@ -6,16 +6,34 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 @Mixin(ServerPlayNetworkHandler.class)
 public abstract class MixinServerPlayNetworkHandler {
+	// in 1.16.3 and earlier, onSignUpdate uses Formatting.strip explicitly
 	@Redirect(
 		method = "onSignUpdate",
 		at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/util/Formatting;strip(Ljava/lang/String;)Ljava/lang/String;"
-		)
+		),
+		require = 0
 	)
 	private String sectionsigns$onSignUpdateStrip(@Nullable String string) {
 		return string;
+	}
+
+	// in 1.16.4 and later, onSignUpdate uses map(Formatting::strip) on a Stream
+	@Redirect(
+		method = "onSignUpdate",
+		at = @At(
+			value = "INVOKE",
+			target = "Ljava/util/stream/Stream;map(Ljava/util/function/Function;)Ljava/util/stream/Stream;"
+		),
+		require = 0
+	)
+	private Stream sectionsigns$onSignUpdateStripMap(Stream stream, Function<String, String> mapper) {
+		return stream;
 	}
 }
