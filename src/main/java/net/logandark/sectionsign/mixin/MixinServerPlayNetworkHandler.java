00bdad13f66d384106a7d196f398a4bb37c792cc
leocth <22406910+LeoCTH@users.noreply.github.com>
2020-11-05 07:40:09 +0800

    fix MixinServerPlayNetworkHandler

--- src/main/java/net/logandark/sectionsign/mixin/MixinServerPlayNetworkHandler.java
+++ src/main/java/net/logandark/sectionsign/mixin/MixinServerPlayNetworkHandler.java
@@ -4,18 +4,24 @@ import net.minecraft.server.network.ServerPlayNetworkHandler;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public abstract class MixinServerPlayNetworkHandler {
-	@Redirect(
+	@ModifyArg(
 		method = "onSignUpdate",
 		at = @At(
 			value = "INVOKE",
-			target = "Lnet/minecraft/util/Formatting;strip(Ljava/lang/String;)Ljava/lang/String;"
-		)
+			target = "Ljava/util/stream/Stream;map(Ljava/util/function/Function;)Ljava/util/stream/Stream;",
+			ordinal = 0
+		),
+		index = 0
 	)
-	private String sectionsigns$onSignUpdateStrip(@Nullable String string) {
-		return string;
+	private <T> Function<? super T, ? extends T> sectionsigns$onSignUpdateStrip(Function<? super T, ? extends T> mapper) {
+		return (unused) -> unused; // identity
 	}
 }
